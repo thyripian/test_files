95d370bf853634ee5c41c40e0e449cd4a0ba96e5
thyripian <110140474+thyripian@users.noreply.github.com>
2023-03-22 13:35:14 -0400

    Add files via upload

---
+++ mongo_db_connection_test.py
@@ -0,0 +1,23 @@
+from sshtunnel import SSHTunnelForwarder
+import pymongo
+import pprint
+
+MONGO_HOST = #IP ADDRESS
+MONGO_DB = #DATABASE NAME
+MONGO_USER = #USERNAME
+MONGO_PASS = #PASSWORD
+
+server = SSHTunnelForwarder(
+    MONGO_HOST,
+    ssh_username = MONGO_USER,
+    ssh_password = MONGO_PASS,
+    remote_bind_address = ('127,0,0,1', 22)
+    )
+    
+server.start()
+
+client = pymongo.MongoClient('127.0.0.1', server.local_bind_port)
+df = client[MONGO_DB]
+pprint.pprint(df.collection_names())
+
+server.stop()
